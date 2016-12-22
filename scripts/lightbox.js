46bcfb82da7d316456a2f3ad8b34d11ec4872cb9
AndShum <ashumway2@gmail.com>
2016-12-21 21:25:01 -0800

    trying different orginization for lightbox code

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,34 +1,33 @@
 
-function handleLightBox() {
-  var $overlay = $('<div id="overlay"></div>');
-  var $image = $('<img>');
+var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
 
+function handleLightBox() {
 
   $overlay.append($image);
 
   $('body').append($overlay);
 
-
-
-  $('.gallery a').click(function(event) {
-  	 event.preventDefault();
-    console.log(this);
-  	 var imageLocation = $(this).attr('href');
-    console.log(this);
-  	 $image.attr('src', imageLocation);
-
-
-  	 $overlay.fadeIn('fast');
-
-    console.log(this);
-
-  });
-
-
-  $overlay.click(function(){
-    $overlay.fadeOut('fast');
-  });
-
 }
 
 handleLightBox();
+
+
+$('.gallery a').click(function(event) {
+  event.preventDefault();
+  console.log(this);
+  var imageLocation = $(this).attr('href');
+  console.log(this);
+  $image.attr('src', imageLocation);
+
+
+  $overlay.fadeIn('fast');
+
+  console.log(this);
+
+});
+
+
+$overlay.click(function(){
+  $overlay.fadeOut('fast');
+});
