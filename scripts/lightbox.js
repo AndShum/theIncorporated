753b60b3be89b6b61b143c3506f86f7823c33992
AndShum <ashumway2@gmail.com>
2016-12-21 21:30:29 -0800

    took it back, didn't work

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,33 +1,34 @@
 
-var $overlay = $('<div id="overlay"></div>');
-var $image = $('<img>');
-
 function handleLightBox() {
+  var $overlay = $('<div id="overlay"></div>');
+  var $image = $('<img>');
+
 
   $overlay.append($image);
 
   $('body').append($overlay);
 
+
+
+  $('.gallery a').click(function(event) {
+  	 event.preventDefault();
+    console.log(this);
+  	 var imageLocation = $(this).attr('href');
+    console.log(this);
+  	 $image.attr('src', imageLocation);
+
+
+  	 $overlay.fadeIn('fast');
+
+    console.log(this);
+
+  });
+
+
+  $overlay.click(function(){
+    $overlay.fadeOut('fast');
+  });
+
 }
 
 handleLightBox();
-
-
-$('.gallery a').click(function(event) {
-  event.preventDefault();
-  console.log(this);
-  var imageLocation = $(this).attr('href');
-  console.log(this);
-  $image.attr('src', imageLocation);
-
-
-  $overlay.fadeIn('fast');
-
-  console.log(this);
-
-});
-
-
-$overlay.click(function(){
-  $overlay.fadeOut('fast');
-});
