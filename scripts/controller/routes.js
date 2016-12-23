5a6b31bb7123d406407887edd15ef317ff2bf4e8
AndShum <ashumway2@gmail.com>
2016-12-23 01:18:11 -0800

    reverted base href changes, still not resolving 404 refresh error

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -1,8 +1,8 @@
-page('https://andshum.github.io/theIncorporated/theIncorporated', collectionsController.reveal);
-page('https://andshum.github.io/theIncorporated/retailers', retailersController.reveal);
-page('https://andshum.github.io/theIncorporated/shop', shopController.reveal);
-page('https://andshum.github.io/theIncorporated/contact', contactController.reveal);
-page('https://andshum.github.io/theIncorporated/andrade_editorial', andradeController.reveal);
-page('https://andshum.github.io/theIncorporated/godoy_lookbook', godoyController.reveal);
+page('/theIncorporated', collectionsController.reveal);
+page('/retailers', retailersController.reveal);
+page('/shop', shopController.reveal);
+page('/contact', contactController.reveal);
+page('/andrade_editorial', andradeController.reveal);
+page('/godoy_lookbook', godoyController.reveal);
 
 page();
